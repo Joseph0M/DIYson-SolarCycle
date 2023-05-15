e213b1182b73e3ec264c2a663ff5e23b5b751871
Joseph0M <joseph.morton875@outlook.com>
2023-05-15 12:14:09 +0100

    Changed endpoints, Added config.json

--- DIYson/Firmware/main.py
+++ DIYson/Firmware/main.py
@@ -2,20 +2,23 @@ import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from diyson_api.routes.api import router as api_router
-app = FastAPI()
+import json
 
-origins = ["http://localhost:8005"]
+with open("DIYson/Firmware/diyson_api/lib/config.json") as json_file: #load config.json
+    config = json.load(json_file)
+if config["LAMP_DATA"]["WIFI"] == True:
+    app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+    origins = ["http://localhost:8005"]
 
-app.include_router(api_router)
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    app.include_router(api_router)
 
-if __name__ == '__main__':
-    uvicorn.run("main:app", host='192.168.0.63', port=8005, log_level="info", reload=True)
-    print("running")
+    if __name__ == '__main__':
+        uvicorn.run("main:app", host=str(config["SERVER_DATA"]["IP"]), port=int(config["SERVER_DATA"]["PORT"]), log_level="info", reload=True)
